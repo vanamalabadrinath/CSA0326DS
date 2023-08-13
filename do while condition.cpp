476e2f778c1e25a5557a088875d04b03464674f5
vanamalabadrinath <130487911+vanamalabadrinath@users.noreply.github.com>
2023-08-13 11:16:01 +0530

    Add files via upload

---
+++ do while condition.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i, n;
+printf("Enter the value of n\t:");
+scanf("%d", &n);
+printf("Printing natural numbers from 1 to %d\n", n);
+i = 1;
+do
+{
+	printf("%d\t", i);
+	i++;
+}
+while(i <= n);
+printf("\n");
+}
