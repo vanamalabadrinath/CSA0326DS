e9086d9c8370fbc488da6a27f24cb588087c9fba
vanamalabadrinath <130487911+vanamalabadrinath@users.noreply.github.com>
2023-08-13 11:09:12 +0530

    Add files via upload

---
+++ print 10 values.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int sum;
+	int i,a[10];
+	for(i=0;i<10;i++)
+	{
+		printf("enter the value:");
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+	printf("%d\n",a[i]);	
+}
+return 0;
+}
